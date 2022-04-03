3df564d6fec01a92035f8c4ccf942c4c7167a298
itallocastro <ipcas@ic.ufal.br>
2022-04-03 14:53:07 -0300

    add config heroku

--- src/config/database.js
+++ src/config/database.js
@@ -2,13 +2,23 @@ require('dotenv').config({
     path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env',
 })
 
-module.exports = {
-    database: process.env.DB_NAME,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    dialect: process.env.DB_DIALECT || 'postgres',
-    port: process.env.DB_PORT,
-    define: {
-        timestamps: true
-    },
+if(process.env.DATABASE_URL) {
+    module.exports = {
+        url: process.env.DATABASE_URL,
+        define: {
+            timestamps: true
+        },
+        dialect: process.env.DB_DIALECT || 'postgres',
+    }
+} else {
+    module.exports = {
+        database: process.env.DB_NAME,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        dialect: process.env.DB_DIALECT || 'postgres',
+        port: process.env.DB_PORT,
+        define: {
+            timestamps: true
+        },
+    }
 }
